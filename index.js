7d9a03f2c08b724bba9406051f1e40aafefe37e9
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2021-08-24 14:45:50 +0700

    feat: add reflected post api

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import shelljs from "shelljs";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.APP_PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "XSS Labs",
+  });
+});
+
+app.post("/api/reflected", async (req, res) => {
+  const { target_url, cookies, payloads } = req.body;
+  try {
+    const parsedPayload = payloads.replace(/\\n/g, "");
+    console.log(parsedPayload);
+    const command = `yarn reflected-cli --target_url=${target_url} --cookies="${cookies}" --payloads="${parsedPayload}"`;
+    const reflected = shelljs.exec(command, { silent: true });
+    const response = {
+      message: "Reflected XSS",
+      data: reflected.stdout,
+    };
+    res.json(response);
+  } catch {
+    res.json({
+      message: "failed",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at ${PORT}`);
+});
+
+// const { code, stdout } = reflected;
+// console.log(`Code: ${code}`);
+// console.log(`Stdout: ${stdout}`);
